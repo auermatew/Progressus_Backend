f1d2d19f81be2e6589498a0e1b78d1adfae9a17b
auer.mate <auermate2005@gmail.com>
2025-02-27 00:41:53 +0100

    init

---
+++ src/test/java/hu/progressus/ProgressusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progressus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressusBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
